f29937e3c4ca57075d6c53e94fe8ef6d7bed252a
Andrey Faychak <faychak333@gmail.com>
2020-04-22 11:29:17 +0300

    Updates

---
+++ precache-manifest.79a24c0324c720f917d2b698510524ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "495f63fd486f14e993436bd825a3695c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bb59236e737c3683e59b",
+    "url": "./static/css/main.381429ce.chunk.css"
+  },
+  {
+    "revision": "b23cd99607aafc7c9c87",
+    "url": "./static/js/2.bc76db86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bc76db86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb59236e737c3683e59b",
+    "url": "./static/js/main.75a752f5.chunk.js"
+  },
+  {
+    "revision": "d34856cc17b978b4a115",
+    "url": "./static/js/runtime-main.9d7a78c6.js"
+  }
+]);
